6ffc19c3e3f6cc0c5167542ee145df0b44a8b35f
lz20r <zhiyaonaiara@gmail.com>
2026-01-11 17:00:29 +0100

    Agregar envío de errores a webhook en manejadores de excepciones no controladas

--- anticrash/antiCrash.js
+++ anticrash/antiCrash.js
@@ -1,13 +1,18 @@
+
 const logger = require('../Util/logger');
+const { sendErrorToWebhook } = require('../Util/webhookError');
 
 module.exports = () => {
 	process.on('unhandledRejection', (reason, p) => {
 		logger.error('Unhandled Rejection/Catch:', reason, p);
+		sendErrorToWebhook('Unhandled Rejection/Catch', reason && reason.stack ? reason.stack : String(reason));
 	});
 	process.on('uncaughtException', (err, origin) => {
 		logger.error('Uncaught Exception/Catch:', err, origin);
+		sendErrorToWebhook('Uncaught Exception/Catch', err && err.stack ? err.stack : String(err));
 	});
 	process.on('uncaughtExceptionMonitor', (err, origin) => {
 		logger.error('Uncaught Exception Monitor:', err, origin);
+		sendErrorToWebhook('Uncaught Exception Monitor', err && err.stack ? err.stack : String(err));
 	});
 };
