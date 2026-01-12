1256af8843a92f6b1bf9f6b6a177db849f25ab23
lz20r <zhiyaonaiara@gmail.com>
2026-01-12 23:29:25 +0100

    Añadir función para crear embeds de unión de miembros en el servidor

---
+++ Util/auditMemberEmbeds.js
@@ -0,0 +1,20 @@
+
+const { ContainerBuilder, TextDisplayBuilder, SeparatorBuilder, MessageFlags } = require('discord.js');
+const MEMBER_COLOR = 0xA259FF; // Morado NQN
+const MEMBER_ICON = 'https://cdn.discordapp.com/emojis/802917097851469834.png';
+const MEMBER_FOOTER = 'AuditLog • MoxiBot';
+
+function memberJoinEmbed({ userId, timeStr }) {
+    const container = new ContainerBuilder()
+        .setAccentColor(MEMBER_COLOR)
+        .addTextDisplayComponents(c => c.setContent(`# Nuevo usuario en el servidor`))
+        .addSeparatorComponents(s => s.setDivider(true))
+        .addTextDisplayComponents(c => c.setContent(`El usuario <@${userId}> se ha unido al servidor.`))
+        .addTextDisplayComponents(c => c.setContent(`Fecha: ${timeStr}`))
+        .addTextDisplayComponents(c => c.setContent(`${MEMBER_FOOTER}`));
+    return { content: '', components: [container], flags: MessageFlags.IsComponentsV2 };
+}
+
+module.exports = {
+    memberJoinEmbed,
+};
