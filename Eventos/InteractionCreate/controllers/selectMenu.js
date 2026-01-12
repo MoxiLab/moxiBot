51fc7b3df55be149448524e2bc861727cb0f789f
lz20r <zhiyaonaiara@gmail.com>
2026-01-12 23:27:52 +0100

    Añadir manejo para el menú de selección de canal en el controlador de menús

--- Eventos/InteractionCreate/controllers/selectMenu.js
+++ Eventos/InteractionCreate/controllers/selectMenu.js
@@ -1,7 +1,10 @@
 // Router for select menu interactions
 module.exports = async function selectMenuController(interaction, Moxi, logger) {
   const helpSelectMenu = require('./selectMenus/help');
+  const channelSelectMenu = require('./selectMenus/channel');
 
   // Help menus
   if (await helpSelectMenu(interaction, Moxi, logger)) return;
+  // Channel menus
+  if (await channelSelectMenu(interaction, Moxi, logger)) return;
 };
