e141e08da3e602625813c64136c85b141674ee8d
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 00:12:06 +0100

    Refactor: agregar manejo de menús de tienda, permisos y bolsa en el controlador de select menu

--- Eventos/InteractionCreate/controllers/selectMenu.js
+++ Eventos/InteractionCreate/controllers/selectMenu.js
@@ -2,9 +2,18 @@
 module.exports = async function selectMenuController(interaction, Moxi, logger) {
   const helpSelectMenu = require('./selectMenus/help');
   const channelSelectMenu = require('./selectMenus/channel');
+  const shopSelectMenu = require('./selectMenus/shop');
+  const permsSelectMenu = require('./selectMenus/perms');
+  const bagSelectMenu = require('./selectMenus/bag');
 
   // Help menus
   if (await helpSelectMenu(interaction, Moxi, logger)) return;
   // Channel menus
   if (await channelSelectMenu(interaction, Moxi, logger)) return;
+  // Shop menus
+  if (await shopSelectMenu(interaction, Moxi, logger)) return;
+  // Perms menus
+  if (await permsSelectMenu(interaction, Moxi, logger)) return;
+  // Bag menus
+  if (await bagSelectMenu(interaction, Moxi, logger)) return;
 };
