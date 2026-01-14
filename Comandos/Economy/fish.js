834caa7c413025e5907c386602657d31d85b4d41
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:45:45 +0100

    Refactor: mejorar la legibilidad del módulo fish al desestructurar la importación

--- Comandos/Economy/fish.js
+++ Comandos/Economy/fish.js
@@ -1,1 +1,7 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'fish', alias: ['pescar'], description: 'Pesca (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'fish',
+    alias: ['pescar'],
+    description: 'Pesca (próximamente)',
+});
