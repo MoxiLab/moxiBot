c886d68543a13e0acb9c0400ec2f36b644b63284
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:48:45 +0100

    Refactor: mejorar la legibilidad del módulo slots al desestructurar la importación

--- Comandos/Economy/slots.js
+++ Comandos/Economy/slots.js
@@ -1,1 +1,6 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'slots', description: 'Tragaperras (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'slots',
+    description: 'Tragaperras (próximamente)',
+});
