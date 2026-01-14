18f8cc860b604c3baab506ee81f3d77b66073cb2
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:46:46 +0100

    Refactor: mejorar la legibilidad del módulo market al desestructurar la importación

--- Comandos/Economy/market.js
+++ Comandos/Economy/market.js
@@ -1,1 +1,6 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'market', description: 'Mercado (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'market',
+    description: 'Mercado (próximamente)',
+});
