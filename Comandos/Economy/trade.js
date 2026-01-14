a54eb94b822948f285aeb9a3eea5bdae0d5f96eb
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:48:55 +0100

    Refactor: mejorar la legibilidad del módulo trade al desestructurar la importación

--- Comandos/Economy/trade.js
+++ Comandos/Economy/trade.js
@@ -1,1 +1,7 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'trade', alias: ['intercambiar'], description: 'Intercambios (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'trade',
+    alias: ['intercambiar'],
+    description: 'Intercambios (próximamente)',
+});
