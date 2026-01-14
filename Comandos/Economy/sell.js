36dececaa223d731ecc69c263bd87da8d0629177
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:48:01 +0100

    Refactor: mejorar la legibilidad del módulo sell al desestructurar la importación

--- Comandos/Economy/sell.js
+++ Comandos/Economy/sell.js
@@ -1,1 +1,7 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'sell', alias: ['vender'], description: 'Vender items (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'sell',
+    alias: ['vender'],
+    description: 'Vender items (próximamente)',
+});
