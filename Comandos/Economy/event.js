2cc32fc7e48e385206364a7a0d423f891e476be1
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:44:50 +0100

    Refactor: mejorar la legibilidad del módulo event al desestructurar la importación

--- Comandos/Economy/event.js
+++ Comandos/Economy/event.js
@@ -1,1 +1,6 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'event', description: 'Eventos (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'event',
+    description: 'Eventos (próximamente)',
+});
