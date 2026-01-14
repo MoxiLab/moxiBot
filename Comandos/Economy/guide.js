065c1237a5ee8fd083cb57d6e768421ed2f79097
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:46:26 +0100

    Refactor: mejorar la legibilidad del módulo guide al desestructurar la importación

--- Comandos/Economy/guide.js
+++ Comandos/Economy/guide.js
@@ -1,1 +1,6 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'guide', description: 'Guía (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'guide',
+    description: 'Guía (próximamente)',
+});
