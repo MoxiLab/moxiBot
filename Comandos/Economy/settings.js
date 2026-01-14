b0ec0d3440d52384b66c04eaf10ad707b98f940c
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:48:19 +0100

    Refactor: mejorar la legibilidad del módulo settings al desestructurar la importación

--- Comandos/Economy/settings.js
+++ Comandos/Economy/settings.js
@@ -1,1 +1,7 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'settings', alias: ['config'], description: 'Ajustes de economía (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'settings',
+    alias: ['config'],
+    description: 'Ajustes de economía (próximamente)',
+});
