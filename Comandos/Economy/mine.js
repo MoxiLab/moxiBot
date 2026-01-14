07197bf11ff501f3ff6755dcbe6352a548451ca6
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:46:53 +0100

    Refactor: mejorar la legibilidad del módulo mine al desestructurar la importación

--- Comandos/Economy/mine.js
+++ Comandos/Economy/mine.js
@@ -1,1 +1,7 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'mine', alias: ['minar'], description: 'Minería (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'mine',
+    alias: ['minar'],
+    description: 'Minería (próximamente)',
+});
