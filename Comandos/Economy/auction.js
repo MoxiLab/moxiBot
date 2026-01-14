510abf14b2dc4b3821fc413c6f06a0a172d8d26e
lz20r <zhiyaonaiara@gmail.com>
2026-01-14 01:43:22 +0100

    Refactor: mejorar la legibilidad del módulo de subastas al desestructurar la importación

--- Comandos/Economy/auction.js
+++ Comandos/Economy/auction.js
@@ -1,1 +1,6 @@
-module.exports = require('../../Util/economyPlaceholders').makePrefixEconomyPlaceholder({ name: 'auction', description: 'Subastas (próximamente)' });
+const { makePrefixEconomyPlaceholder } = require('../../Util/economyPlaceholders');
+
+module.exports = makePrefixEconomyPlaceholder({
+    name: 'auction',
+    description: 'Subastas (próximamente)',
+});
