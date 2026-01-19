189b29c02f624c08a21e150b726a4b21739a6cdb
lz20r <zhiyaonaiara@gmail.com>
2026-01-19 10:24:49 +0100

    feat: cargar .env para inicializar variables de entorno antes de la gestión de shards

--- sharder.js
+++ sharder.js
@@ -1,6 +1,10 @@
 const { ShardingManager } = require('discord.js');
 const logger = require('./Util/logger');
 const { EMOJIS } = require('./Util/emojis');
+
+// Cargar .env lo antes posible para TOKEN y resto de vars.
+require('./Util/silentDotenv')();
+
 const manager = new ShardingManager('./index.js', {
     token: process.env.TOKEN,
     totalShards: 'auto'
