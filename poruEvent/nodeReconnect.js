e87aead9928980d627d24a38f270885ccfb6ccca
lz20r <zhiyaonaiara@gmail.com>
2026-01-25 13:58:29 +0100

    feat: mejorar el manejo de eventos de depuración en nodeReconnect para registrar información detallada

--- poruEvent/nodeReconnect.js
+++ poruEvent/nodeReconnect.js
@@ -1,9 +1,11 @@
 const logger = require("../Util/logger");
 
 module.exports = (client, node) => {
+    if (!logger.isDebugFlagEnabled('poru')) return;
+
     try {
         const nodeName = node?.name ?? node?.options?.name ?? "(unknown)";
-        logger.warn(`[PORU] nodeReconnect: ${nodeName}`);
+        logger.debug(`[PORU] nodeReconnect: ${nodeName}`);
     } catch (e) {
         logger.error("[PORU] nodeReconnect handler error", e);
     }
